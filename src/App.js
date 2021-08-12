4a9d89bfab2ef3c94eb586852a0e68f36459c711
oyershov <oyershov.work@gmail.com>
2021-08-12 22:21:30 +0200

    Feature: add capacitor example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Iframe from 'react-iframe';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Iframe url="https://puginarug.com/"
+        width="100%"
+        height="100%"
+        id="myId"
+        display="initial"
+        position="relative"
+      />
     </div>
   );
 }
